911074b8491bea37afed53ebab5c059e491a2d59
3e - Zajac <3e-zajac@42-4.local>
2022-03-22 12:18:31 +0100

    22.03 v2

--- profile/profile.js
+++ profile/profile.js
@@ -2,4 +2,9 @@ window.onload = function() {
   $(".icon").click(function()  {
     $("#exampleModal").modal("toggle");
   });
+  let photo = document.getElementById("loadFile").files[0];
+  let formData = new FormData();
+
+  formData.append("photo", photo);
+  fetch('../res/profpic/test', {method: "POST", body: formData});
 };
